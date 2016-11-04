7a647b6a78a485786ce84394ea197b3e796f299e
tobins <tobins@gmail.com>
2016-11-04 15:32:53 -0700

    Initial Commit

---
+++ UserTap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserTap
+//
+//  Created by Tobin Schwaiger-Hastanan on 11/4/16.
+//  Copyright © 2016 Tobin Schwaiger-Hastanan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
